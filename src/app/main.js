abac923b12a3f989a6d27ec8ece7b73066361ca1
Dmitriy <shkredovdmitriy@gmail.com>
2022-06-29 13:11:34 +0300

    feat: update heder link, update animation

--- src/app/main.js
+++ src/app/main.js
@@ -1,6 +1,6 @@
 import '../styles/scss/main.scss';
 
 gsap.registerPlugin(DrawSVGPlugin);
-gsap.from('.figure-1', { duration: 3, stagger: 0.1, drawSVG: 0 });
-gsap.from('.figure-2', { duration: 3, stagger: 0.1, drawSVG: 0 });
-gsap.from('.figure-3', { duration: 3, stagger: 0.1, drawSVG: 0 });
+gsap.from('.figure-1', { duration: 4, stagger: 0.1, drawSVG: 0 });
+gsap.from('.figure-2', { duration: 4, stagger: 0.1, drawSVG: '50% 50%' });
+gsap.from('.figure-3', { duration: 4, stagger: 0.1, drawSVG: 0 });
